761b8af305c4f9c4f67a3a7caf3dbc86895e199d
Jorge Bustamante <74831269+joelouisbm@users.noreply.github.com>
2024-11-03 18:34:14 -0500

    Update GenericRepository.java

--- src/main/java/co/edu/cun/virtualbook/repository/GenericRepository.java
+++ src/main/java/co/edu/cun/virtualbook/repository/GenericRepository.java
@@ -3,16 +3,13 @@ package co.edu.cun.virtualbook.repository;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface GenericRepository <T, ID>{
+public interface GenericRepository <T, ID> {
 
-    T create( T entity )
-        throws SQLException;
-    List<T> getAll()
-        throws SQLException;
+    T create(T entity) throws SQLException;
+    
+    List<T> getAll() throws SQLException;
 
-    T update( T entity )
-        throws SQLException;
+    T update(T entity)throws SQLException;
 
-    void delete( ID id )
-        throws SQLException;
+    void delete(ID id) throws SQLException;
 }
