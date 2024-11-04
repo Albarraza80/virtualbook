17f1bf3ce8f886545b2d5388d88c40554c029d0f
Jorge Bustamante <74831269+joelouisbm@users.noreply.github.com>
2024-11-04 13:23:53 -0500

    Update Application.java

--- src/main/java/co/edu/cun/virtualbook/Application.java
+++ src/main/java/co/edu/cun/virtualbook/Application.java
@@ -1,18 +1,5 @@
 package co.edu.cun.virtualbook;
 
-import co.edu.cun.virtualbook.form.BookForm;
-import co.edu.cun.virtualbook.service.BookService;
-
-import java.io.IOException;
-import java.sql.SQLException;
-import javax.swing.JFrame;
-
-public class Main{
-    public static void main( String[] args )
-        throws IOException, SQLException{
-        BookForm book = new BookForm();
-        book.setTitle("book");
-        book.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        book.setVisible(true);
-    }
+public class Application {
+    public static void main( String[] args ) {}
 }
