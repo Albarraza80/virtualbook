0b21c86a3f46795e16f1acffecb9486726e3a8b8
Jorge Bustamante <74831269+joelouisbm@users.noreply.github.com>
2024-11-03 20:34:24 -0500

    Se agregan entidades

--- src/main/java/co/edu/cun/virtualbook/domain/Author.java
+++ src/main/java/co/edu/cun/virtualbook/domain/Author.java
@@ -1,9 +1,110 @@
 package co.edu.cun.virtualbook.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Author {
-    
+    private Integer id;
     private String name;
     private String lastname;
-    private Integer brithYear;
+    private Integer birthYear;
+    private String nationality;
+    private String biography;
+    private String birthPlace;
+    private List<Book> books; // Relación muchos a muchos con Book
 
+    public Author(String name, String lastname, Integer birthYear, String nationality, String biography, String birthPlace) {
+        this.name = name;
+        this.lastname = lastname;
+        this.birthYear = birthYear;
+        this.nationality = nationality;
+        this.biography = biography;
+        this.birthPlace = birthPlace;
+        this.books = new ArrayList<>();
+    }
+
+    public Author(Integer id, String name, String lastname, Integer birthYear, String nationality, String biography, String birthPlace) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.birthYear = birthYear;
+        this.nationality = nationality;
+        this.biography = biography;
+        this.birthPlace = birthPlace;
+        this.books = new ArrayList<>();
+    }
+
+    // Getters y Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(Integer birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public String getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    // Método para añadir un libro a la lista
+    public void addBook(Book book) {
+        if (!books.contains(book)) {
+            books.add(book);
+            book.getAuthorList().add(this); // Añade el autor al libro también
+        }
+    }
 }
--- src/main/java/co/edu/cun/virtualbook/domain/Book.java
+++ src/main/java/co/edu/cun/virtualbook/domain/Book.java
@@ -1,78 +1,94 @@
 package co.edu.cun.virtualbook.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Book{
+public class Book {
     private int id;
     private String title;
     private String genre;
     private Integer pages;
     private int publicYear;
     private String editorial;
-    private List<Author> authorList;
+    private List<Author> authorList; // Relación muchos a muchos con Author
 
-    public Book(int id, String title, String genre) {
+    public Book() {
+        this.authorList = new ArrayList<>();
+    }
+
+    public Book(int id, String title, String genre, Integer pages, int publicYear, String editorial) {
         this.id = id;
         this.title = title;
         this.genre = genre;
+        this.pages = pages;
+        this.publicYear = publicYear;
+        this.editorial = editorial;
+        this.authorList = new ArrayList<>();
     }
 
-    public Book(){
-    }
+    // Getters y Setters
 
-    public int getId(){
+    public int getId() {
         return id;
     }
 
-    public void setId( int id ){
+    public void setId(int id) {
         this.id = id;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return title;
     }
 
-    public void setTitle( String title ){
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public String getGenre(){
+    public String getGenre() {
         return genre;
     }
 
-    public void setGenre( String genre ){
+    public void setGenre(String genre) {
         this.genre = genre;
     }
 
-    public Integer getPages(){
+    public Integer getPages() {
         return pages;
     }
 
-    public void setPages( Integer pages ){
+    public void setPages(Integer pages) {
         this.pages = pages;
     }
 
-    public int getPublicYear(){
+    public int getPublicYear() {
         return publicYear;
     }
 
-    public void setPublicYear( int publicYear ){
+    public void setPublicYear(int publicYear) {
         this.publicYear = publicYear;
     }
 
-    public List<Author> getAuthorList(){
-        return authorList;
-    }
-
-    public void setAuthorList( List<Author> authorList ){
-        this.authorList = authorList;
-    }
-
-    public String getEditorial(){
+    public String getEditorial() {
         return editorial;
     }
 
-    public void setEditorial( String editorial ){
+    public void setEditorial(String editorial) {
         this.editorial = editorial;
+    }
+
+    public List<Author> getAuthorList() {
+        return authorList;
+    }
+
+    public void setAuthorList(List<Author> authorList) {
+        this.authorList = authorList;
+    }
+
+    // Método para añadir un autor a la lista
+    public void addAuthor(Author author) {
+        if (!authorList.contains(author)) {
+            authorList.add(author);
+            author.getBooks().add(this); // Añade el libro a la lista de libros del autor también
+        }
     }
 }
